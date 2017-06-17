a2bff399d7c9e764633c6e6bdc52210b03b94e1e
Dan Murphy <danwright91@gmail.com>
2017-06-17 17:44:52 +0100

    Added working delta time

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate specs;
+extern crate time;
 
-use specs::{Component, DispatcherBuilder, VecStorage, ReadStorage, WriteStorage, System, World};
-use std::{thread, time};
+use specs::{Component, DispatcherBuilder, Fetch, VecStorage, ReadStorage, WriteStorage, System, World};
+use std::{thread};
+
 
 #[derive(Debug)]
 struct Position {
-    x: f32,
-    y: f32
+    x: f64,
+    y: f64
 }
 
 impl Component for Position {
@@ -15,8 +17,8 @@ impl Component for Position {
 
 #[derive(Debug)]
 struct Velocity {
-    x: f32,
-    y: f32
+    x: f64,
+    y: f64
 }
 
 impl Component for Velocity {
@@ -40,24 +42,31 @@ impl<'a> System<'a> for LoggingSystem {
 struct UpdatePositionSystem;
 
 impl<'a> System<'a> for UpdatePositionSystem {
-    type SystemData = (ReadStorage<'a, Velocity>, WriteStorage<'a, Position>);
+    type SystemData = (Fetch<'a, DeltaTime>,
+        ReadStorage<'a, Velocity>,
+        WriteStorage<'a, Position>);
 
     fn run(&mut self, data: Self::SystemData) {
         use specs::Join;
 
-        let (vel, mut pos) = data;
+        let (delta, vel, mut pos) = data;
+
+        let delta = delta.0.num_milliseconds() as f64;
 
         for (vel, pos) in (&vel, &mut pos).join() {
-            pos.x += vel.x * 0.05;
-            pos.y += vel.y * 0.05;
+            pos.x += vel.x * delta;
+            pos.y += vel.y * delta;
         }
     }
 }
+
+struct DeltaTime(time::Duration);
 
 fn main() {
     let mut world = World::new();
     world.register::<Position>();
     world.register::<Velocity>();
+    world.add_resource(DeltaTime(time::Duration::from_std(std::time::Duration::new(0, 0)).unwrap()));
 
     let ball = world.create_entity()
         .with(Position {x: 4.0, y: 7.0})
@@ -72,9 +81,18 @@ fn main() {
         .add(update_position_system, "update_position_system", &["logging_system"])
         .build();
 
-    let sleep_duration = time::Duration::from_millis(1000);
+    let sleep_duration = std::time::Duration::from_millis(1000);
+    let mut last_update = std::time::SystemTime::now();
     loop {
+        {
+            let mut delta = world.write_resource::<DeltaTime>();
+            let last_update_elapsed = last_update.elapsed().unwrap();
+            println!("{:?}", last_update_elapsed);
+            *delta = DeltaTime(time::Duration::from_std(last_update_elapsed).unwrap());
+        }
+
         dispatcher.dispatch(&mut world.res);
+        last_update = std::time::SystemTime::now();
         thread::sleep(sleep_duration);
     }
 }
